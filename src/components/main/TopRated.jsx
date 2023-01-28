9c51669c3d1db0d83a78d6dfc88f97fba2724e23
AFRIKAN BACKEND DEVELOPER <christopherleon237@gmail.com>
2023-01-28 04:34:23 +0100

    done

--- src/components/main/TopRated.jsx
+++ src/components/main/TopRated.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useEffect } from 'react'
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import Placeholder from "../../assets/blank.png";
 
 function TopRated() {
     const [Data, setData] = useState([])
     const topRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=75a2f69d31ae8a963d53a3e8b2a9943a&language=en-US&page=1"
-    const img_path="https://image.tmdb.org/t/p/original"
-    
+    const img_path = "https://image.tmdb.org/t/p/original"
+
     useEffect(() => {
         axios.get(topRated)
             .then((res) => {
@@ -33,7 +35,12 @@ function TopRated() {
                 {Data.map((obj) => {
                     return (
                         <div className="card flex flex-col md:gap-2 gap-4 md:w-56 mb-8 transition ease-in-out delay-75 hover:-translate-y-0 hover:scale-105 duration-1000">
-                            <img src={obj.image} alt="" className='border rounded-xl h-60 w-full object-cover' />
+                            <LazyLoadImage src={obj.image}
+                                PlaceholderSrc={Placeholder}
+                                alt="Image Alt"
+                                className='border rounded-xl h-60 w-full object-cover'
+                            />
+                            {/* <img src={obj.image} alt="" className='border rounded-xl h-60 w-full object-cover' /> */}
                             <h2 className='text-sm font-bold'>{obj.title}</h2>
                             <div className="info flex flex-row  justify-between">
                                 <p className='text-sm'>Release Date: {obj.date}</p>
