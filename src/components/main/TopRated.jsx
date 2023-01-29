4be39ca1903c487e9dc56b700c8395ef9dca2044
AFRIKAN BACKEND DEVELOPER <christopherleon237@gmail.com>
2023-01-29 21:51:04 +0100

    done

--- src/components/main/TopRated.jsx
+++ src/components/main/TopRated.jsx
@@ -7,9 +7,27 @@ import { Link } from "react-router-dom";
 
 function TopRated() {
     const [Data, setData] = useState([])
+    const [visible, setVisible]=useState(8)
+    const [less, setLess]=useState(false)
+
     const topRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=75a2f69d31ae8a963d53a3e8b2a9943a&language=en-US&page=1"
     const img_path = "https://image.tmdb.org/t/p/original"
 
+    const showMore= (()=>{
+        setVisible((more) => more + 8)
+        console.log(visible)
+        console.log(Data.length)
+        if (Math.floor (visible / Data.length) >= 1){
+            setLess(true)
+        }
+    })
+    const showLess= (()=>{
+        if (visible === 8){
+            setLess(false)
+        }else{
+            setVisible((more) => more - 8)
+        }
+    })
     useEffect(() => {
         axios.get(topRated)
             .then((res) => {
@@ -27,13 +45,13 @@ function TopRated() {
             .catch((err) => { console.log(err) })
     }, [])
     return (
-        <div className=''>
+        <div className='flex flex-col justify-center place-content-center'>
             <div className="heading flex flex-row gap-20 justify-around px-2">
                 <h2 className='text-2xl font-semibold'>Top Rated</h2>
                 <h3 className='text-xl text-red-400 cursor-pointer'>View All</h3>
             </div>
             <div className="list flex md:flex-row flex-col flex-wrap pt-12 md:justify-between px-2">
-                {Data.map((obj) => {
+                {Data.slice(0, visible).map((obj) => {
                     return (
                         <Link to={`details/${obj.id}`}>
                             <div className="card flex flex-col md:gap-2 gap-4 md:w-56 mb-8 transition ease-in-out delay-75 hover:-translate-y-0 hover:scale-105 duration-1000 cursor-pointer">
@@ -52,6 +70,10 @@ function TopRated() {
                         </Link>
                     )
                 })}
+            </div>
+            <div className='flex place-content-center '>
+                {!less ?<button className='text-xl py-2 w-1/4 border rounded-lg text-white text-center bg-emerald-600' onClick={showMore}>More</button>
+                :<button className='text-xl py-2 w-1/4 border rounded-lg text-white text-center bg-emerald-600' onClick={showLess}>Less</button>}
             </div>
         </div>
     )
